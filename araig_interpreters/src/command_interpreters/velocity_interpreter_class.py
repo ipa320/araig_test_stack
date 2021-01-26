98e4c08594bcd19ebbb4945371fb43882abf7f49
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2021-01-27 00:39:57 +0100

    vel_interpreter add reset, latch, loginfo

--- araig_interpreters/src/command_interpreters/velocity_interpreter_class.py
+++ araig_interpreters/src/command_interpreters/velocity_interpreter_class.py
@@ -4,37 +4,27 @@ import threading
 from araig_msgs.msg import BoolStamped
 from geometry_msgs.msg import Twist
 
-class CallbackList():
-    def callback_start(self, msg, args):
-        VelocityInterpreterClass.LOCK[args].acquire()
-        VelocityInterpreterClass.DATA[args] = msg.data
-        VelocityInterpreterClass.LOCK[args].release()
-
-    def callback_stop(self, msg, args):
-        VelocityInterpreterClass.LOCK[args].acquire()
-        VelocityInterpreterClass.DATA[args] = msg.data
-        VelocityInterpreterClass.LOCK[args].release()   
-
 class VelocityInterpreterClass():
     LOCK = {}
     DATA = {}
     def __init__(self,
-        pub_msg,
+        max_linear_x,
         loop_rate_hz = 1):
 
         self.pub_topic = "/velocity"
-        self.sub_list = ["/start", "/stop"]
+        self.sub_topic_start = "/start"
+        self.sub_topic_stop = "/stop"
 
-        self.pub_msg = pub_msg
+        self.max_linear_x = max_linear_x
         self.rate = rospy.Rate(loop_rate_hz)
 
         self.pub_init()
         self.sub_init()
 
         # init FLAG and LOCK
-        for topic in self.sub_list:
-            VelocityInterpreterClass.DATA[topic] = False
-            VelocityInterpreterClass.LOCK[topic] = threading.Lock()
+        for topic in [self.sub_topic_start, self.sub_topic_stop]:
+            self.DATA[topic] = False
+            self.LOCK[topic] = threading.Lock()
         
         self.main()
         
@@ -42,39 +32,60 @@ class VelocityInterpreterClass():
         self.pub_diag = rospy.Publisher(
                 self.pub_topic, 
                 Twist, 
-                queue_size=10 
+                queue_size=10,
+                latch = True
             )
 
-    def sub_init(self):
-        self.sub_diag = {}
+    def callback_start(self, msg, args):
+        with self.LOCK[args]:
+            self.DATA[args] = msg.data
 
-        for topic in self.sub_list:
-           
-            call_back_name = topic.replace('/','_')
-            
-            callback = getattr(CallbackList(), 'callback'+ call_back_name)
-            self.sub_diag[topic] = rospy.Subscriber(topic, BoolStamped, callback, (topic))
+    def callback_stop(self, msg, args):
+        with self.LOCK[args]:
+            self.DATA[args] = msg.data
+
+    def sub_init(self):
+        sub_diag = {}
+        sub_diag[self.sub_topic_start] = rospy.Subscriber(self.sub_topic_start, BoolStamped, self.callback_start, (self.sub_topic_start))
+        sub_diag[self.sub_topic_stop] = rospy.Subscriber(self.sub_topic_stop, BoolStamped, self.callback_start, (self.sub_topic_stop))
+
       
     def main(self):
         flag_both_high = False
+        pre_start = False 
+        pre_stop = False
+        msg = Twist()
         try:
             while not rospy.is_shutdown():
                 temp = {}
 
-                for topic in self.sub_list:
-                    VelocityInterpreterClass.LOCK[topic].acquire()
-                    temp[topic]= VelocityInterpreterClass.DATA[topic]
-                    VelocityInterpreterClass.LOCK[topic].release()
+                for topic in [self.sub_topic_start, self.sub_topic_stop]:
+                    with self.LOCK[topic]:
+                        temp[topic]= self.DATA[topic]
 
-                if temp["/start"] == 1 and temp["/stop"] == 0:
-                    self.pub_diag.publish(self.pub_msg["/start"])
-        
-                if temp["/start"] == 0 and temp["/stop"] == 1:
-                    self.pub_diag.publish(self.pub_msg["/stop"])
+                if temp[self.sub_topic_start] == 1 and temp[self.sub_topic_stop] == 0:
+                    msg.linear.x = self.max_linear_x
+                    self.pub_diag.publish(msg)
+                    if pre_start == False:
+                        rospy.loginfo(rospy.get_name() +": Send velocity({}) to robot...".format(msg.linear.x)) 
+                        pre_start = True       
+                    
+                if temp[self.sub_topic_start] == 0 and temp[self.sub_topic_stop] == 1:
+                    msg.linear.x = 0
+                    self.pub_diag.publish(msg)
+                    if pre_stop == False:
+                        rospy.loginfo(rospy.get_name() +": Stop robot...") 
+                        pre_stop = True       
 
-                if temp["/start"] == 1 and temp["/stop"] == 1 and flag_both_high == False:
+                if temp[self.sub_topic_start] == 1 and temp[self.sub_topic_stop] == 1 and flag_both_high == False:
                     rospy.logwarn(rospy.get_name() +": Both  START and STOP signals are high")
                     flag_both_high = True
-                self.rate.sleep()   
+
+                if pre_stop == True and pre_stop == True and temp[self.sub_topic_start] == 0 and temp[self.sub_topic_stop] == 0:
+                    # rospy.logwarn(rospy.get_name() +": reset velocity interpreter")
+                    pre_start = False 
+                    pre_stop = False
+                
+            self.rate.sleep()   
         except rospy.ROSException:
             pass
