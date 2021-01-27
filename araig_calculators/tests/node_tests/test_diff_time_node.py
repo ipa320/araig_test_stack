80968d097a84179334dde9bd144b2616ef89d9b2
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2021-01-27 22:08:00 +0100

    fix error when stop signal before start signal

--- araig_calculators/tests/node_tests/test_diff_time_node.py
+++ araig_calculators/tests/node_tests/test_diff_time_node.py
@@ -8,14 +8,14 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from  araig_msgs.msg import BoolStamped, Float64Stamped
 
-class TestCalcStopWatch(unittest.TestCase):
+class TestDiffTime(unittest.TestCase):
 
     def setUp(self):
         _pub_topic_1 = '/test/start'
         _pub_topic_2 = '/test/stop'
         _sub_topic_1 = '/test/duration'
 
-        rospy.init_node('test_calc_stop_watch', anonymous=True)
+        rospy.init_node('test_diff_time', anonymous=True)
         self.pub_start = rospy.Publisher(_pub_topic_1, BoolStamped, latch=True, queue_size=10)
         self.pub_stop = rospy.Publisher(_pub_topic_2, BoolStamped, latch= True, queue_size=10)
 
@@ -23,46 +23,62 @@ class TestCalcStopWatch(unittest.TestCase):
 
         self.duration = None
         self.msg_seq = 0
+        self.count = 0
 
     def test_expected(self):
+        while self.count < 3:
+            self.count += 1
+            # pub stop signal first
+            pub_signal = BoolStamped()
+            pub_signal.header.stamp = rospy.Time.now()
+            pub_signal.data = True
+            self.pub_stop.publish(pub_signal)
+            rospy.sleep(1)
+
+            # pub start signal
+            pub_signal.header.stamp = rospy.Time.now()
+            pub_signal.data = True
+            self.pub_start.publish(pub_signal)
+            rospy.sleep(0.1)
+            # reset stop signal
+            pub_signal.data = False
+            self.pub_stop.publish(pub_signal)
+            rospy.sleep(0.3)
+
+            pub_signal.header.stamp = rospy.Time.now()
+            pub_signal.data = True
+            self.pub_stop.publish(pub_signal)
+        
+            while self.duration is None:
+                time.sleep(0.1)
+            self.assertAlmostEqual(self.duration, 0.4, None, msg='test{}: {} is not equal to 0.3'.format(self.count, self.duration), delta=0.05)
+            self.assertEqual(self.msg_seq, self.count, 'msg published {} times'.format(self.msg_seq))
+
+            # reset
+            self.duration = None
+            pub_signal.data = False
+            pub_signal.header.stamp = rospy.Time.now()
+            self.pub_start.publish(pub_signal)
+            rospy.sleep(0.1)
+            pub_signal.header.stamp = rospy.Time.now()
+            self.pub_stop.publish(pub_signal)
+            rospy.sleep(0.1)
 
         # pub start signal
-        pub_signal = BoolStamped()
         pub_signal.header.stamp = rospy.Time.now()
         pub_signal.data = True
         self.pub_start.publish(pub_signal)
+        rospy.sleep(0.1)
         rospy.sleep(0.3)
 
         pub_signal.header.stamp = rospy.Time.now()
         pub_signal.data = True
         self.pub_stop.publish(pub_signal)
-        
+    
         while self.duration is None:
             time.sleep(0.1)
-        self.assertAlmostEqual(self.duration, 0.3, None, msg='1st test: {} is not equal to 0.3'.format(self.duration), delta=0.05)
-        self.assertEqual(self.msg_seq, 1, 'msg published {} times'.format(self.msg_seq))
-
-        self.duration = None
-        pub_signal.data = False
-        pub_signal.header.stamp = rospy.Time.now()
-        self.pub_start.publish(pub_signal)
-        rospy.sleep(0.1)
-        pub_signal.header.stamp = rospy.Time.now()
-        self.pub_stop.publish(pub_signal)
-        rospy.sleep(0.1)
-
-        pub_signal.header.stamp = rospy.Time.now()
-        pub_signal.data = True
-        self.pub_start.publish(pub_signal)
-        rospy.sleep(0.1)
-
-        pub_signal.header.stamp = rospy.Time.now()
-        pub_signal.data = True
-        self.pub_stop.publish(pub_signal)
-        while self.duration is None:
-            time.sleep(0.1)
-        self.assertAlmostEqual(self.duration, 0.1, None, msg='2nd test: {} is not equal to 0.1'.format(self.duration), delta=0.05)
-        self.assertEqual(self.msg_seq, 2, 'msg published {} times'.format(self.msg_seq))
+        self.assertAlmostEqual(self.duration, 0.4, None, msg='test{}: {} is not equal to 0.3'.format(self.count+1, self.duration), delta=0.05)
+        self.assertEqual(self.msg_seq, self.count+1, 'msg published {} times'.format(self.msg_seq))
 
     def callback_1(self, msg):
         self.duration = msg.data
@@ -70,5 +86,5 @@ class TestCalcStopWatch(unittest.TestCase):
 
 if __name__ == '__main__':
     pkg = 'araig_calculators'
-    name = 'test_calc_stop_watch'
-    rostest.rosrun(pkg, name, TestCalcStopWatch)
+    name = 'test_diff_time'
+    rostest.rosrun(pkg, name, TestDiffTime)
