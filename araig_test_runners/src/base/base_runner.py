1b80f89ae1b2a16c61fa16033d7e4d23792d71b7
ipa-rwu <ruichao.wu@ipa.fraunhofer.de>
2021-02-04 22:45:32 +0100

    base_runner: noblock, check condition, rosparam

--- araig_test_runners/src/base/base_runner.py
+++ araig_test_runners/src/base/base_runner.py
@@ -6,7 +6,7 @@ import os
 import threading
 
 class TestBase(object):
-    def __init__(self, sub_dict = {} , pub_dict = {}, rate = 100):      
+    def __init__(self, sub_dict = {} , pub_dict = {}, param_list = {}, rate = 100):      
 
         self._rate = rospy.Rate(rate)
 
@@ -23,37 +23,56 @@ class TestBase(object):
             "test_failed"           : "/signal/runner/test_failed",
             "test_succeeded"        : "/signal/runner/test_succeeded",
             "start_test"            : "/signal/ui/start_test",
-            "reset_test"            : "/signal/ui/reset_test"
+            "reset_test"            : "/signal/ui/reset_test",
+            "interrupt_test"        : "/signal/ui/interrupt_test"
         }
         self._output_interface.update(pub_dict)
+        self._config_param = []
+        self._config_param += param_list
 
         self._locks = {}
         self._flag = {}
         self._publishers = {}
 
+        # get ros param:
+        self.config_param = {}
+        self.get_config(self._config_param)
+
         # sub_init
         for key in self._input_interface:
             rospy.Subscriber(self._input_interface[key], BoolStamped, self.callback_for_all_bool_topics, key)
             self._locks[key] = threading.Lock()
-            self._flag[key] = False
-
+            self._flag[key] = BoolStamped()
+            self._flag[key].data = False
         # pub_init
         for key in self._output_interface:
             self._publishers[key] = rospy.Publisher(self._output_interface[key], BoolStamped,queue_size=10, latch=True)
-   
+
+        try:
+            while not rospy.is_shutdown():
+                self.main() 
+                self._rate.sleep()
+        except rospy.ROSException:
+            pass
+
     def setSafeFlag(self, key, value):
         if not key in self._input_interface.keys():
             rospy.logerr(rospy.get_name() + ": Retrieving a key that does not exist!: {}".format(key))
             return
         with self._locks[key]:
             self._flag[key] = value
-
-    def getSafeFlag(self, key):
+    
+    # If seq = False, get data; If True, get header
+    def getSafeFlag(self, key, header = False):
         if not key in self._input_interface.keys():
             rospy.logerr(rospy.get_name() + ": Retrieving a key that does not exist!: {}".format(key))
             return
-        with self._locks[key]:
-            return self._flag[key]
+        else:
+            with self._locks[key]:
+                if not header:
+                    return self._flag[key].data
+                else:
+                    return self._flag[key].header
     
     def buildNewBoolStamped(self, data = True):
         msg = BoolStamped()
@@ -62,8 +81,31 @@ class TestBase(object):
         return msg
 
     def callback_for_all_bool_topics(self, msg, key):
-        self.setSafeFlag(key,msg.data)
+        self.setSafeFlag(key,msg)
 
     def startRecordingAndWait(self, duration=3):
         rospy.loginfo(rospy.get_name() + ": waiting for {}s, then start recoding ...".format(duration))
         rospy.sleep(duration)
+
+    def timestampToFloat(self, stamp):
+        timestamp_float =  float(stamp.secs + float(stamp.nsecs*(1e-9)))
+        return timestamp_float
+
+    def checkCondition(self, start_timestamp, duration):
+        if self.timestampToFloat(rospy.Time.now() - start_timestamp) <= duration:
+            return False
+        else:
+            return True
+    
+    def get_config(self, param_list):
+        for arg in param_list:
+            module_name = "/runner/"
+            ns = module_name
+            if rospy.has_param(ns + arg):
+                self.config_param[arg] = rospy.get_param(ns + arg)
+            else:
+                rospy.logerr("{}: {} param not set!!".format(ns, arg))
+                rospy.signal_shutdown()
+    
+    def main(self):
+        pass
