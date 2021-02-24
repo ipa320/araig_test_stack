9a3b77dc5316b6763be5d35712032868433bc7df
PN:Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2021-02-24 12:33:03 +0100

    rosbagger based on base_logger_class

--- araig_calculators/src/loggers/rosbagger_class.py
+++ araig_calculators/src/loggers/rosbagger_class.py
@@ -3,95 +3,73 @@
 import rospy
 import subprocess, shlex
 from araig_msgs.msg import BoolStamped
+from base_classes.base_logger import BaseLogger
 import threading
 
-class RosbaggerClass(object):
-    def __init__(self,  start_topic = "/start",
-                        stop_topic = "/stop",
-                        stop_offset = 2,
-                        blacklist_topics = None,
-                        whitelist_topics = None,
-                        filename ="test"):
-        self.filename_ = filename 
-        self._stop_offset = stop_offset
-        self.start_mutex_ = threading.Lock()
-        self.stop_mutex_ = threading.Lock()
-        self._start_recording = False
-        self._stop_recording_ = False
-        self._rate = rospy.Rate(10)
+class RosbaggerClass(BaseLogger):
+    def __init__(self):
 
-        rospy.Subscriber(start_topic, BoolStamped, self.start_callback)
-        rospy.Subscriber(stop_topic, BoolStamped, self.stop_callback)
+        self.node_name = rospy.get_name()
+        param_list = [
+            self.node_name + "/start_offset",
+            self.node_name + "/stop_offset",
+            self.node_name + "/blacklist",
+            self.node_name + "/whitelist",
+            "/test_type"]
 
-        self.blacklist_topics = blacklist_topics
-        self.whitelist_topics = whitelist_topics
+        super(RosbaggerClass, self).__init__(param_list = param_list)
 
         try:
             while not rospy.is_shutdown():
-                self.main_loop()
+                self.main_loop() 
         except rospy.ROSException:
-            pass  
-
-    def __del__(self):
-        rospy.loginfo(rospy.get_name() + ": Destructor killing process {}".format(self._rosbag_proc))
-        self._rosbag_proc.send_signal(subprocess.signal.SIGINT)
-
-    def stop_callback(self, msg):
-            with self.stop_mutex_:
-                self._stop_recording_ = msg.data
-
-    def start_callback(self, msg):
-            with self.start_mutex_:
-                self._start_recording = msg.data
+            pass 
+    
+    def prepare_topics(self):
+        list_of_topics = rospy.get_published_topics()
+        list_of_topics.sort()
+        topics_string = ""
+        for topic, msg in list_of_topics:
+            for black_topic in self.config_param[self.node_name + "/blacklist"]:
+                if topic == black_topic:
+                    topic = ""
+            for white_topic in self.config_param[self.node_name + "/whitelist"]:
+                if topic == white_topic:
+                    topic = ""
+            topics_string = topics_string + topic + " "
+        
+        for white_topic in self.config_param[self.node_name + "/whitelist"]:
+            topics_string = topics_string + white_topic + " "
+        
+        return topics_string
 
     def main_loop(self):
-        rospy.logwarn(rospy.get_name() + ": Waiting for start signal...")
+        rospy.loginfo(rospy.get_name() + ": Waiting for start signal...")
         # Wait for start signal
         start = False
         stop = False
         while not start and not rospy.is_shutdown():
             self._rate.sleep()
-            with self.stop_mutex_:
-                start = self._start_recording
+            start = self.getSafeFlag("start")
     
          # get published topic
         if start == True and stop == False:
-            list_of_topics = rospy.get_published_topics()
-            list_of_topics.sort()
-            _topics_string = ""
-            for topic, msg in list_of_topics:
-                for black_topic in self.blacklist_topics:
-                    if topic == black_topic:
-                        topic = ""
-                for white_topic in self.whitelist_topics:
-                    if topic == white_topic:
-                        topic = ""
-                _topics_string = _topics_string + topic + " "
-            
-            for white_topic in self.whitelist_topics:
-                _topics_string = _topics_string + white_topic + " "
+            rospy.sleep(self.config_param[self.node_name + "/start_offset"])
+            currentFolder = self.getSubFolder() 
+
+            topics_string = self.prepare_topics()
                 
             # Prepare command
-            self._command = "rosbag record -o " + self.filename_ + " " + _topics_string
-
-            command = shlex.split(self._command)
-            self._rosbag_proc = subprocess.Popen(command)
-            rospy.logwarn(rospy.get_name() + ": Starting recording now, process{}!".format(self._rosbag_proc))
+            command = "rosbag record -o " + currentFolder + "/" + self.config_param["/test_type"] + " " + topics_string
+            rospy.loginfo(rospy.get_name() + ": " + command)
+            self.startCommandProc(command)
 
         # Wait for stop signal
-        with self.stop_mutex_:
-                stop = self._stop_recording_
-        while not stop:
+        while not stop and not rospy.is_shutdown():
             self._rate.sleep()
-            with self.stop_mutex_:
-                stop = self._stop_recording_
-            if rospy.is_shutdown():
-                rospy.loginfo(rospy.get_name() + ": Got kill signal, ending process {}".format(self._rosbag_proc))
-                self._rosbag_proc.send_signal(subprocess.signal.SIGINT)
-                rospy.loginfo(rospy.get_name() + ": Process killed")
-                return
-        rospy.logwarn(rospy.get_name() + ": Got kill signal, ending process {} after {}s"
-                                        .format(self._rosbag_proc, self._stop_offset))
-        rospy.sleep(rospy.Duration(self._stop_offset))
-        self._rosbag_proc.send_signal(subprocess.signal.SIGINT)
-        rospy.logwarn(rospy.get_name() + ": Process killed")
+            stop = self.getSafeFlag("stop")
+        
+        if start == True:
+            if stop or rospy.is_shutdown():
+                rospy.sleep(self.config_param[self.node_name + "/stop_offset"])
+                self.killCommandProc()
