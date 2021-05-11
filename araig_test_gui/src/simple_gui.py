4d0a689a1b431c550923e1fef1f4dc05c5a88208
ipa-alb <aulon.bajrami@ipa.fraunhofer.de>
2021-05-11 18:10:55 +0200

    testing seg fault fixes

--- araig_test_gui/src/simple_gui.py
+++ araig_test_gui/src/simple_gui.py
@@ -58,6 +58,11 @@ class Led(Image):
     def set_off(self):
         self.state='off'
     
+    def set_on(self):
+        print('before setstate')
+        self.state='on'
+        print('after setstate')
+
     def on_source_on(self,instance,src):
         if self.state == 'on' and (self.led_type == 'source' or self.led_type == 'both'):
             self.source=self.source_on
@@ -96,40 +101,49 @@ class App(MDApp):
         self.screen=Builder.load_file('ros_gui.kv')
         self.mutex_lock_float_1 = threading.Lock()
         self.mutex_lock_float_2 = threading.Lock()
-        rospy.Subscriber('/float_1', Float64Stamped,  self.callback_float_1)
+        rospy.Subscriber('/float_1', Float64Stamped, self.callback_float_1)
         rospy.Subscriber('/float_2', Float64Stamped, self.callback_float_2)
+
+        rospy.Subscriber('/bool_1', BoolStamped, self.callback_test_completed)
+        rospy.Subscriber('/bool_2', BoolStamped, self.callback_test_failed)
+        rospy.Subscriber('/bool_3', BoolStamped, self.callback_test_succeeded)
+
         self.float_1 = 0
         self.float_2 = 1
         print('called constructor')
 
-        """rospy.Subscriber('/signal/runner/test_completed', self.callback_test_completed)
-        self.mutex_lock = threading.lock()
-        self.completed
-
-        rospy.Subscriber('/signal/runner/test_succeeded', self.callback_test_succeeded)
-        self.mutex_lock_2 = threading.lock()
-        self.succeeded  """ 
     def callback_float_1(self, msg):
-        print('asdf')
-        with self.mutex_lock_float_1():
-            print("Got data: {}".format(msg.data))
-            self.root.ids['result1_v_label'].text = msg.data
+        # print('asdf')
+        
+        self.root.ids['result1_v_label'].text = str(msg.data)
 
     def callback_float_2(self, msg):
-        with self.mutex_lock_float_2():
-            self.root.ids['result2_v_label'].text = msg.data
+       
+        self.root.ids['result2_v_label'].text = str(msg.data)
 
     def callback_test_completed(self, msg):
-        with self.mutex_lock():
-            self.completed = msg.data
+       
+        print("Got data: {}".format(msg.data))
+        
+        self.completed = msg.data
+        if self.completed == True:
+            print('it became true')
+            self.root.ids['led_typeboth1'].set_on()
+
 
     def callback_test_failed(self, msg):
-        with self.mutex_lock():
-            self.failed = msg.data
+        
+        self.failed = msg.data
+        if self.failed == True:
+            self.root.ids['led_typeboth2'].set_on()
+
 
     def callback_test_succeeded(self, msg):
-        with self.mutex_lock_2():
-            self.succeeded = msg.data
+        
+        
+        self.succeeded = msg.data
+        if self.suceeded == True:
+            self.root.ids['led_typeboth3'].set_on()
 
     def update_led(self):
         with self.mutex_lock_2():
@@ -158,47 +172,34 @@ class App(MDApp):
     
     def press_start(self,*args):
 
-
-        msg=BoolStamped() #alte
+        msg=BoolStamped()
         msg.header.stamp = rospy.Time.now() 
         msg.data = True
-        pub1.publish(msg) 
-        if self.testsetup == 'test1_breaking':
-            print(self.testsetup +" has been started")
-            #sub
+        pub1.publish(msg)
 
-        elif self.testsetup == 'test1_emergency':
-            print(self.testsetup +" has been started")
-            #
-
-        elif self.testsetup == 'test4':
-            print(self.testsetup +" has been started")
-            #
-
-        elif self.testsetup == 'test5_with_nav':
-            print(self.testsetup +" has been started")
-
-        else:
-            print(self.testsetup +" has been started")
-
-        
-        
-        self.root.ids['led_animated'].toggle_state()
+        self.root.ids['led_animated'].set_on()
 
     def press_interrupt(self,*args):
+
+        msg=BoolStamped()
+        msg.header.stamp = rospy.Time.now() 
+        msg.data = True
+
         print('Interrupt button pressed')
-        msg=True
         pub2.publish(msg)
-        self.root.ids['led_animated'].set_off()
-        self.root.ids['led_typeboth1'].toggle_state()
+        #self.root.ids['led_animated'].set_off()
+        #self.root.ids['led_typeboth1'].toggle_state()
     
     
     def press_reset(self,*args):
+
         print('Reset button pressed')
-        msg=True
+        msg=BoolStamped()
+        msg.header.stamp = rospy.Time.now() 
+        msg.data = True
         pub3.publish(msg)
-        self.root.ids['led_animated'].set_off()
-        self.root.ids['led_typeboth1'].toggle_state()
+        #self.root.ids['led_animated'].set_off()
+        #self.root.ids['led_typeboth1'].toggle_state()
 
 
     def spinner_clicked(self,value):
@@ -247,6 +248,8 @@ if __name__ == '__main__':
     pub1=rospy.Publisher('/button1',BoolStamped,queue_size=1)
     pub2=rospy.Publisher('/button2',BoolStamped,queue_size=1)
     pub3=rospy.Publisher('/button3',BoolStamped,queue_size=1)
+
+    
     rospy.init_node('simple_gui',anonymous=True)
     spin_thread = threading.Thread(target=lambda: rospy.spin())
     spin_thread.start()
